e061d8ee37e125870918b2c69fa733af4e3341b7
Felipe <felipe.j03@edu.pucrs.br>
2026-05-25 16:12:47 -0300

    feat(application): add low stock alert use case port (Epic 4)

--- src/main/java/com/nostrapizza/estoque_api/application/port/in/ListLowStockProductsUseCase.java
+++ src/main/java/com/nostrapizza/estoque_api/application/port/in/ListLowStockProductsUseCase.java
@@ -1,4 +1,9 @@
 package com.nostrapizza.estoque_api.application.port.in;
 
+import com.nostrapizza.estoque_api.domain.entity.Product;
+
+import java.util.List;
+
 public interface ListLowStockProductsUseCase {
+    List<Product> execute();
 }
